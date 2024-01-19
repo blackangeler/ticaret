3ea0da61fe21cbafe3d9c1345451162d90e6e1aa
blackangeler <61097399+blackangeler@users.noreply.github.com>
2024-01-19 14:16:02 +0300

    Add files via upload

---
+++ conn.php
@@ -0,0 +1,10 @@
+<?php
+// Veritabanı bağlantısı
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "products";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+?>
